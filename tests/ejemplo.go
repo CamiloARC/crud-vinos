aa6dc2c512a43ed5f15d4abcad94347eed966804
CamiloARC <camilo._car@hotmail.com>
2024-04-21 17:38:39 -0500

    se agrega buildspec.yml

---
+++ tests/ejemplo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func IsSuperAnimal(animal string) bool {
+	return strings.ToLower(animal) == "gopher"
+}
+
+func TestIsSuperAnimal(t *testing.T) {
+	expected := true
+	got := IsSuperAnimal("gopher")
+	if got != expected {
+		t.Errorf("Expected: %v, got: %v", expected, got)
+	}
+}
